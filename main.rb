92895b2b36ec1d772f28a7ff20cfadf7c21b54c9
Abdul Munim Kazia <munim2020@gmail.com>
2015-01-02 16:59:58 +0530

    cleanup

--- main.rb
+++ main.rb
@@ -1,18 +1,21 @@
 require "./rover.rb"
 
 File.foreach('input.txt').with_index { |line, line_num|
-	if line_num == 0
+	
+  if line_num == 0
 		coords = line.split(' ').map { |x| x.to_i } 
 		$r = Rover.new(coords[0], coords[1])
 		next
 	end
 	
-	if line_num % 2 == 1
+	#Odd lines have starting position
+  if line_num % 2 == 1
 		start = line.split(' ')
 		$r.set_position start[0].to_i, start[1].to_i, start[2]
 	end 
 
-	if line_num %2 == 0
+  #Even lines have direction
+	if line_num % 2 == 0
 		line.chomp.each_char { |char|
 			if char == 'M'
 				$r.move
--- rover.rb
+++ rover.rb
@@ -3,7 +3,7 @@ class Rover
 	attr_reader :right
 	attr_reader :top
 	
-	def initialize(x, y)
+	def initialize x, y
 		@right = x
 		@top = y
 		@rover = {
@@ -13,50 +13,57 @@ class Rover
 		}
 	end
 
-	def set_position(x, y, direction)
+  # Resets the rover's position and direction
+	def set_position x, y, direction
 		@rover[:x] = x
 		@rover[:y] = y
 		@rover[:direction] = direction.downcase.to_sym
 	end
 
+  # Moves the rover forward
 	def move
 		case @rover[:direction]
-			when :n
-				if @rover[:y] < @top
-					@rover[:y] += 1
-				end
-			when :w
-				if @rover[:x] > 0
-					@rover[:x] -= 1
-				end
-			when :s
-				if @rover[:y] > 0
-					@rover[:y] -= 1
-				end
-			when :e
-				if @rover[:x] < @right
-					@rover[:x] += 1
-				end
-		end
-	end
+      when :n
+        if @rover[:y] < @top
+          @rover[:y] += 1
+        end
 
-	def turn(new_direction)
-		directions = [:n, :e, :s, :w]
-		index = directions.index(@rover[:direction])
-		if new_direction == 'L'
-			index -=1
-		else
-			index += 1
-		end
-		if index > 3 
-			index = (index+1) % 4 - 1
-		end
-		@rover[:direction] =  directions[index]
+      when :w
+        if @rover[:x] > 0
+          @rover[:x] -= 1
+        end
 
-	end
+      when :s
+        if @rover[:y] > 0
+          @rover[:y] -= 1
+        end
 
-	def to_s
-		@rover[:x].to_s + ' ' + @rover[:y].to_s + ' ' + @rover[:direction].to_s.upcase
-	end
+      when :e
+        if @rover[:x] < @right
+          @rover[:x] += 1
+        end
+    end
+  end
+
+  # Turns the rover. Argument can be 'L' or 'R'
+  def turn new_direction
+    directions = [:n, :e, :s, :w]
+    index = directions.index(@rover[:direction])
+    if new_direction == 'L'
+      index -=1
+    else
+      index += 1
+    end
+    if index > 3 
+      index = (index+1) % 4 - 1
+    end
+  @rover[:direction] =  directions[index]
+
+  end
+
+  # Simple helper which returns the rover's current position and direction as a string
+  def to_s
+    @rover[:x].to_s + ' ' + @rover[:y].to_s + ' ' + @rover[:direction].to_s.upcase
+  end
 
 end
